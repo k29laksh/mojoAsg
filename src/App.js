f4a557406b669abace705e7fa8e273028a8dd779
k29laksh <klaxay0505@gmail.com>
2024-07-08 22:22:30 +0530

    mojoasg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import FacebookLogin from 'react-facebook-login';
 import './App.css';
 
 function App() {
+
+  // const [login, setLogin] = useState(false);
+  // const [data, setData] = useState({});
+  // const [picture, setPicture] = useState('');
+
+  // const responseFacebook = (response) => {
+  //   console.log(response);
+  //   setData(response);
+  //   setPicture(response.picture.data.url);
+  //   if (response.accessToken) {
+  //     setLogin(true);
+  //   } else {
+  //     setLogin(false);
+  //   }
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      {/* <div style={{ width: '600px' }}>
+        <h1>
+          {
+           <FacebookLogin
+              appId="414597001568100"
+              autoLoad={true}
+              fields="name,email,picture"
+              scope="public_profile" 
+              callback={responseFacebook}
+              icon="fa-facebook" />
+          }
+          {login &&
+            <img src={picture} roundedCircle />
+          }
+        </h1>
+        {login &&
+          <div>
+            <p>{data.name}</p>
+            <div>
+              {data.email}
+            </div>
+          </div>
+        }
+      </div> */}
     </div>
   );
 }
